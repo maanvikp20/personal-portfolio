a18c65a6749453ba6dd96aea588a0e4bca629826
maanvikp20 <maanvikpoddar@gmail.com>
2024-07-03 13:18:40 -0700

    combined and removed extra files + beautify

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navRight = document.querySelector('.nav-right');
-
-    menuToggle.addEventListener('click', function() {
-        navRight.classList.toggle('active');
-    });
-});
